8efb34d9ba8ad49bcaf451a40988b20883dba2a8
JoshFarwig <54459705+JoshFarwig@users.noreply.github.com>
2025-06-03 21:54:23 -0600

    added alembic, still need to create first migration

--- back-end/app/core/database.py
+++ back-end/app/core/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 import os
 
-# TODO Look into how fastapi handles configurations and environment variables
+# TODO refactor to use pydantic settings manager,
+# generate database url via method from config
 DATABASE_URL = os.getenv(
     "DATABASE_URL", "postgresql+asyncpg://user:pass@localhost:5432/mydb"
 )
@@ -10,12 +11,12 @@ DATABASE_URL = os.getenv(
 engine = create_async_engine(
     DATABASE_URL,
     echo=True,
-    pool_size=10,  # cntrols how many connections in pool
+    pool_size=10,  # controls how many connections in pool
     max_overflow=5,  # extra connections allowed temporarily
-    pool_timeout=30,  # seconds to wait before error if pool is full
+    pool_timeout=30,  # seconds to wait before error if pool is full,
 )
 
-# session factory
+# session factory, need to double check if I should be expiring on session commit or not.
 AsyncSessionLocal = async_sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
