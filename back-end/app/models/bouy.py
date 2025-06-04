3b19bc3d4d76ef1f1f49274e9cbc39ad6d0c854d
JoshFarwig <54459705+JoshFarwig@users.noreply.github.com>
2025-06-03 21:44:53 -0600

    commenting out other models till I hash out some basics

--- back-end/app/models/bouy.py
+++ back-end/app/models/bouy.py
@@ -1,8 +1,8 @@
-from typing import Optional, Union, List
-from sqlalchemy import ForeignKey, String
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from .base import Base
+# from typing import Optional, Union, List
+# from sqlalchemy import ForeignKey, String
+# from sqlalchemy.orm import Mapped, mapped_column, relationship
+# from .base import Base
 
 
-class Bouy(Base):
-    __tablename__ = "bouys"
+# class Bouy(Base):
+#     __tablename__ = "bouys"
