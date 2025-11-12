a90aa9a4ace5be2149d229358915fdc0fdd3a574
JoshFarwig <54459705+JoshFarwig@users.noreply.github.com>
2025-11-12 12:42:10 -1000

    refactor(worker): added basic celery WorkerState class, looking into signals etc

--- backend/celery/worker.py
+++ backend/celery/worker.py
@@ -1,2 +1,78 @@
+import asyncio
+import logging
+
+from celery.signals import worker_init, worker_shutdown, setup_logging
+
+from core import AsyncDatabaseManager, AsyncRedisManager, get_settings, BaseConfig
+from core.logging import configure_logging
+
+logger = logging.getLogger(__name__)
 
 
+class WorkerState:
+    """Singleton resources for a worker instance"""
+
+    # NOTE: the WorkerState singleton is NOT threadsafe.
+    # if celery is initialized / cleaned up with a concurrency option that
+    # uses multithreading, (i.e. -pool=evenlet) this will need
+    # to implement a threading lock to protect race conditions.
+    # refer to https://docs.celeryq.dev/en/v5.5.3/userguide/concurrency/index.html
+    # for concurrency options avaiable to celery.
+
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.initialized = False
+        return cls._instance
+
+    def initialize(self, settings: BaseConfig):
+        if not self.initialized:
+            self.settings = settings
+            self.db_manager = AsyncDatabaseManager(settings.db)
+            self.redis_manager = AsyncRedisManager(
+                settings.redis, settings.redis.get_cache_url()
+            )
+            # TODO: create http_manager
+
+            self.loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(self.loop)
+
+            self.initialized = True
+            logger.info("Worker state initialized")
+
+    async def cleanup(self):
+        if self.initialized:
+            await self.db_manager.close()
+            await self.redis_manager.close()
+            # TODO: create http_manager
+            self.initialized = False
+
+
+# Worker signals
+@worker_init.connect
+def init_worker(**kwargs):
+    """Initialize worker resources"""
+    import os
+
+    env = os.getenv("ENV", "local")
+    settings = get_settings(env)
+    WorkerState().initialize(settings)
+
+
+@setup_logging.connect
+def setup_logging(**kwargs):
+    """Override celery's logging"""
+    import os
+
+    env = os.getenv("ENV", "local")
+    configure_logging(env)
+
+
+@worker_shutdown.connect
+def shutdown_worker(**kwargs):
+    """Cleanup worker resources"""
+    state = WorkerState()
+    if state.initialized:
+        state.loop.run_until_complete(state.cleanup())
--- docs/nwps_exploration.md
+++ docs/nwps_exploration.md
@@ -1,8 +1,8 @@
 # NOAA's NWPS (nearshore wave prediction system)
 
-## the data
+## NOMADS portal, the model, and the data
 
-accessible via [nomads prod url](https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwps/prod), provides grib2 files annd includes model generations for each region, split into specific [sites](https://www.census.gov/topics/preparedness/related-sites/nws.html) (hawaii is HFO)
+accessible via [nomads prod url](https://nomads.ncep.noaa.gov/pub/data/nccf/com/nwps/prod), provides grib2 files and includes model generations for each region, split into specific [sites](https://www.census.gov/topics/preparedness/related-sites/nws.html) (hawaii is HFO)
 
 each site runs models at different times. i.e. for HFO its 00 and 12.
 they will also include may include more than one grid or section. the online
@@ -10,10 +10,13 @@ model viewer portal makes it a bit easier to figure out which CG (computational 
 what section. I.e. for [Maui](https://polar.ncep.noaa.gov/nwps/nwpsloop.php?site=HFO&cg=4), the CG=4.
 
 unforunately no OPenDAP portal for the NWPS, I assume it would be difficult with diferent sites
-having different requirements or data needs for their SWAN models.
+having different requirements or data needs for their SWAN models, does include grib filter and https access
 
 model runs include 2d wave spectral files for noaa bouys that exist
 in the region. not sure if I will need this or not.
 
 grib2 files for hawaii range from 50-100MB due to being an island chain,
 requiring a denser computational grid to portray accurate representations.
+
+grib filters do exist! -> [url](https://nomads.ncep.noaa.gov/gribfilter.php?ds=prnwps), for more [info](https://nomads.ncep.noaa.gov/info.php?page=gribfilter)
+use partial http-transfer method for just specific parameters.
